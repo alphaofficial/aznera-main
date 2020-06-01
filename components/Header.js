14a921acea47c43a2cbd0fabfcbff18035df82fe
Albert Alpha <baronofficial@gmail.com>
2020-06-01 11:02:36 +0000

    fixes

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,10 @@ export default function Header(){
         <header class="header h-fullscreen overflow-hidden bg-size-contain bg-img-right" style={{paddingBottom: '0px', paddingTop: '58px'}}>
             <div class="container-fluid">
             <div class="row h-100" style={{padding: '0px'}}>
-                <div class="col-3" style={{height: '100vh',  margin: '0px', padding: '0px', background: 'url(/assets/img/homepage01.png) no-repeat', backgroundSize: 'cover'}}>
+                <div class="col-md-3 col-sm-12" style={{height: '100vh',  margin: '0px', padding: '0px', background: 'url(/assets/img/homepage01.png) no-repeat', backgroundSize: 'cover'}}>
                     <a href="/discover" className="overlay"><h2 className="title">Discover Azmera</h2></a>
                 </div>
-                <div class="col-2">
+                <div class="col-md-2 col-sm-12">
                     <div class="row">
                         <div className="col-12" style={{height: '50vh',  margin: '0px', padding: '0px', background: 'url(/assets/img/homepage01.png) no-repeat', backgroundSize: 'cover'}}>
                             <a href="/menu" className="overlay"><h2 className="title">Menu</h2></a>
@@ -17,10 +17,10 @@ export default function Header(){
                         </div>
                     </div>
                 </div>
-                <div class="col-4"  style={{height: '100vh',  margin: '0px', padding: '0px', background: 'url(/assets/img/homepage02.png) no-repeat', backgroundSize: 'cover'}}>
+                <div class="col-md-4 col-sm-12"  style={{height: '100vh',  margin: '0px', padding: '0px', background: 'url(/assets/img/homepage02.png) no-repeat', backgroundSize: 'cover'}}>
                     <a href="/menu" className="overlay"><h2 className="title">Gallery</h2></a>
                 </div>
-                <div class="col-3"  style={{height: '100vh',  margin: '0px', padding: '0px', background: 'url(/assets/img/homepage03.png) no-repeat', backgroundSize: 'cover'}}>
+                <div class="col-md-3 col-sm-12"  style={{height: '100vh',  margin: '0px', padding: '0px', background: 'url(/assets/img/homepage03.png) no-repeat', backgroundSize: 'cover'}}>
                         <a href="/contact" className="overlay"><h2 className="title">Contact</h2></a>
                 </div>
             </div>
