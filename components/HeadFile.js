c81c06eaf8d2c4a9c140690a8c75d4e8ab06f77b
Albert Alpha <baronofficial@gmail.com>
2020-06-01 10:22:48 +0000

    bug fixes

--- components/HeadFile.js
+++ components/HeadFile.js
@@ -1,4 +1,4 @@
-import Head from 'next/Head'
+import Head from 'next/head'
 
 export default function HeadFile(){
 
@@ -8,7 +8,6 @@ export default function HeadFile(){
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
             <meta name="description" content=""/>
             <meta name="keywords" content=""/>
-
             <title>Azmera</title>
             <link rel="stylesheet" href="/assets/css/page.min.css" />
             <script type="text/javascript" src="/assets/js/page.min.js"></script>
