ac453870f6fbb31fb2366f29d58dcfe08bd108b5
Albert Alpha <baronofficial@gmail.com>
2020-07-17 00:19:53 +0000

    updates

--- components/HeadFile.js
+++ components/HeadFile.js
@@ -13,7 +13,18 @@ export default function HeadFile(){
             <link href="https://restaurantguru.com/css/badge/delivery_takeaway/delivery_rect_large.css" rel="stylesheet"/>
             <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
             <script type="text/javascript" src="/assets/js/page.min.js"></script>
-            
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-172879360-1"/>
+            <script
+            dangerouslySetInnerHTML={{
+                __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+        
+                    gtag('config', 'UA-172879360-1');
+                `,
+            }}
+            />
         </Head>
     )
 }
